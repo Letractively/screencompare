8ef60e67703d9442bc58f2ba892ce09e07a49575
retoonline <retoonline@c92b2d83-b290-56e9-8038-717fa1db5d29>
2010-09-07 16:41:59 +0000

    Updating release scripts

--- mkBuild.sh
+++ mkBuild.sh
@@ -3,3 +3,6 @@
 cp=$(echo lib/*.jar lib/selenium-1/*.jar lib/selenium-2/*.jar | sed -e 's# #:#g')
 
 javac -d bin -classpath $cp -sourcepath src src/screencompare/ScreencompareTestCase.java src/screencompare/driver/DriverSelenium1.java src/screencompare/driver/DriverSelenium2.java
+
+cd bin
+jar -cf screencompare.jar screencompare
